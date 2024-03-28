af4388c3bfc67fc7346edb567783cdd27931143d
noclipping <devonselvaggi@gmail.com>
2024-03-27 22:53:32 -0400

    prod

--- app.py
+++ app.py
@@ -49,4 +49,4 @@ def delete_user(id):
     return jsonify({'message': 'User deleted successfully'}), 204
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.1', port=5000, debug=True)
